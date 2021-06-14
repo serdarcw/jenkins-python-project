15d64111f5f45e2fc547a39537aec7afea6ae4a1
serdarcw <serkangumus06@gmail.com>
2021-06-15 02:31:42 +0300

    host

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 
 # Configure mysql database
-app.config['MYSQL_DATABASE_HOST'] =  os.getenv('MYSQL_DATABASE_HOST')  # "database-42.cbanmzptkrzf.us-east-1.rds.amazonaws.com"
+app.config['MYSQL_DATABASE_HOST'] =  "ecr-docker-jenkins-rds.cbanmzptkrzf.us-east-1.rds.amazonaws.com" #os.getenv('MYSQL_DATABASE_HOST') 
 app.config['MYSQL_DATABASE_PASSWORD'] =  os.getenv('MYSQL_DATABASE_PASSWORD')  # "Clarusway"
 app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_DATABASE_USER')  # "admin"
 app.config['MYSQL_DATABASE_DB'] =  os.getenv('MYSQL_DATABASE_DB')  # "phonebook"
